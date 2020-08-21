683294433a26027e56e2268cb1298697344e1d40
Paulo Netto <pauloestrella1994@gmail.com>
2020-08-21 10:14:49 -0300

    Loading data in forms

---
+++ js/crud.js
@@ -0,0 +1,73 @@
+/* crud api */
+function read_api(id){
+
+}
+function create_api(data){
+    let dataJson = toJson(data);
+    delete dataJson.id;
+    save(JSON.stringify(dataJson));
+    $('.msg.success').html('<p>Dado salvo com sucesso!</p>');
+}
+function update_api(data) {
+    let id = data[0].value;
+    let dataJson = toJson(data);
+    delete dataJson.id;
+    update(dataJson, id);
+    $('.msg.success').html('<p>Dado editado com sucesso!</p>');
+}
+function delete_api(id){
+    delete_data(id);
+    $('.msg.success').html('<p>Dado deletado com sucesso!</p>');
+}
+//------------------ end crud api
+
+function loadResource(url, selector){
+    $(selector).load(url).promise().done(()=>{
+        index = url.indexOf('?')
+        if(index!=-1){
+            id = url.substring(index+4);
+            findById(id);
+        }
+    });
+}
+
+function btnClick(event){
+    event.preventDefault();
+    url = $(event.target).attr('href');
+    resource = '#form';
+    loadResource(url, resource);
+    $('#form').show(); 
+}
+
+$('.btn-new').click( (event)=>btnClick(event) );
+
+function btnDelete(event) {
+    event.preventDefault();
+    let id = $(event.target).parent().data("id");
+    delete_api(id);
+}
+
+$('form').submit((event)=>{
+    $('.msg.success').html('')
+    $('.msg-error').html('');
+    event.preventDefault();
+    let message = '';
+    let values = $(event.target).serializeArray();
+
+    values.forEach(e => {
+
+        $("[name='"+e['name']+"']").removeClass('input-error');
+        if(e['value'].trim() == '' && e['name'] != 'id') {
+            message += '<p>* O campo '+e['name']+' precisa ser preenchido! </p>';
+            $("[name='"+e['name']+"']").addClass('input-error');
+        }
+    });
+    $('.msg-error').html(message);
+    if(message == ''){
+        if (values[0].value) {
+            update_api(values);
+        } else {
+            create_api(values);
+        }
+    }
+});
---
+++ js/sellers.js
@@ -0,0 +1,114 @@
+var sellers_api = 'http://127.0.0.1:5000/api/sellers/'
+
+function load_data_json(){
+    $.ajax({
+        url : sellers_api
+        ,dataType : 'json'
+        ,type : 'get'
+        ,success: (data)=>{
+            load_data(data);
+        },
+        error: (e) =>{
+            $('.msg.error.error.api').html('<h4> Erro ao acessar a api</h4>')
+        }
+    });
+}
+// ============ END Load data Json File
+
+function update(data, id) {
+    $.ajax({
+        type : 'PUT',
+        url: sellers+id,
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: () => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+
+function toJson(data) {
+    let obj = {};
+    obj['brand'] = data[0].value;
+    obj['business_type'] = data[1].value;
+    obj['cnpj'] = data[2].value;
+    obj['company_name'] = data[3].value;
+    obj['mobile_phone'] = data[4].value;
+    return obj
+}
+// ============ Load Json result in HTML
+
+function load_data(data){
+    data.forEach(e => {
+        data += `<tr>
+            <td>${e['brand']}</td>
+            <td>${e['business_type']}</td>"
+            <td>${e['cnpj']}</td>"
+            <td>${e['company_name']}</td>"
+            <td>${e['mobile_phone']}</td>"
+            <td data-id="${e['id']}">
+                <a class='btn-edit' href='product-rating/form.html?id=${e['id']}'>Editar</a> |
+                <a class='btn-delete' href='#'>Deletar</a>
+            </td>
+        </tr>`;
+    });
+
+        $('table tbody').html(data).promise().done(()=>{
+            $('.btn-edit').click( (event)=>btnClick(event) );
+            $('.btn-delete').click( (event)=>btnDelete(event) );
+        });
+}
+// ============ END Load Json result in HTML
+
+
+function delete_data(id){
+    $.ajax({
+        url : sellers + id
+        , type: 'DELETE'
+        , success: (data)=> {
+            load_data_json();
+        },
+        error: (e)=> {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')            
+        }
+    })
+}
+
+// ============ Find id in Json File and Load html
+function findById(id){
+    $.ajax({
+        url : sellers+id
+        ,dataType : 'json'
+        ,type : 'get'
+        ,success: (data)=>{
+            $("[name='id']").val(data.id);
+            $("[name='brand']").val(data.brand);
+            $("[name='business_type']").val(data.business_type);
+            $("[name='cnpj']").val(data.cnpj);
+            $("[name='company_name']").val(data.company_name);
+            $("[name='mobile_phone']").val(data.mobile_phone);
+        }
+    });
+}
+
+// ============ END Find id in Json File and Load html
+
+function save(data) {
+    $.ajax({
+        type : 'POST',
+        url: sellers,
+        contentType: 'application/json',
+        data: data,
+        success: () => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+
+$(document).ready(()=>load_data_json());
